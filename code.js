0b920424041fa41f053c5038d0d6e7a79563d13b
lecuyerjean <jean171199@gmail.com>
2024-11-12 10:11:09 +0100

    first try with getData

---
+++ code.js
@@ -0,0 +1,10 @@
+const dataUrl = 'https://gist.githubusercontent.com/baiello/0a974b9c1ec73d7d0ed7c8abc361fc8e/raw/e598efa6ef42d34cc8d7e35da5afab795941e53e/recipes.json';
+
+const loading_complete = new Event('recettes fully fetched');
+
+export function getData(){
+    return fetch(dataUrl)
+        .then (Response => Response.json())
+        .then(recettes => recettes)
+        .catch(error => error)
+}
