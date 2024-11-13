8fb27bf7eb802814502f5ebd79e02d14de955074
lecuyerjean <jean171199@gmail.com>
2024-11-13 16:06:03 +0100

    the lists are now without any doubles

--- filtering.js
+++ filtering.js
@@ -8,9 +8,17 @@ function listIngredientCreate(){
     
     for (const ingredient of collection){
         const element_liste=document.createElement('option');
+        var bool = true;
         element_liste.value = ingredient.innerHTML;
         element_liste.textContent = ingredient.innerHTML;
+        for (const nodouble of liste_ingredient){
+            if (nodouble.innerHTML===ingredient.innerHTML){
+                bool=false;
+            }
+        }
+        if (bool){
         liste_ingredient.appendChild(element_liste);
+        }
     }
     return liste_ingredient;
 }
@@ -21,9 +29,17 @@ function listUstensilsCreate(){
     
     for (const ustensiles of collection){
         const element_liste=document.createElement('option');
+        var bool = true;
         element_liste.value = ustensiles.innerHTML;
         element_liste.textContent = ustensiles.innerHTML;
+        for (const nodouble of liste_ustensiles){
+            if (nodouble.innerHTML===ustensiles.innerHTML){
+                bool=false;
+            }
+        }
+        if (bool){
         liste_ustensiles.appendChild(element_liste);
+        }
     }
     return liste_ustensiles;
 }
@@ -33,14 +49,38 @@ function listAppareilsCreate(){
     const liste_appareils = document.getElementById('liste_appareils');
     
     for (const appareils of collection){
-        const element_liste=document.createElement('option');
+        var element_liste=document.createElement('option');
+        var bool = true;
         element_liste.value = appareils.innerHTML;
         element_liste.textContent = appareils.innerHTML;
+        for (const nodouble of liste_appareils){
+            if (nodouble.innerHTML===appareils.innerHTML){
+                bool=false;
+            }
+        }
+        if (bool){
         liste_appareils.appendChild(element_liste);
+        }
+        
     }
     return liste_appareils;
 }
 
-listAppareilsCreate();
-listIngredientCreate();
-listUstensilsCreate();
+// function sortList(html){
+//     var sorted = document.getElementById();
+//     for (const i of html){
+//         var temp = i.innerHTML;
+//         console.log(i, sorted)
+//         for (const j of sorted){
+//             if (i===j){
+//                 temp=''
+            
+//             }
+//         }
+//         sorted+=temp;
+//     }
+//     return sorted
+// }
+// sortList(listAppareilsCreate())
+// sortList(listIngredientCreate())
+// sortList(listUstensilsCreate())
